78fb208aefcc066447f4b79fb46edf07acefbecd
Erik Jacobsen <slowerthanyou@gmail.com>
2017-05-23 08:24:14 -0700

    adding reducer

--- ali_todo_list/client/src/index.js
+++ ali_todo_list/client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../src/Components/App'
-
 import {Provider} from 'react-redux'
 import store from './store'
 
--- ali_todo_list/client/src/reducers/messaging.js
+++ ali_todo_list/client/src/reducers/messaging.js
@@ -2,11 +2,20 @@ const initialState = {
     messages: []
 }
 
-export default function messageReducer(state=initialState, action) {
-    switch (action.type) {
-        case 'ADD_MESSAGE':
-            return {...state, messages: [...state.messages, action.message]}
-        default:
-            return state
-    }
+// export default function messageReducer(state=initialState, action) {
+//     switch (action.type) {
+//         case 'ADD_MESSAGE':
+//             return {...state, messages: [...state.messages, action.message]}
+//         default:
+//             return state
+//     }
+// }
+
+export default function todoReducer(state=initialState, action) {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return {...state, toDos: [...state.toDos, action.toDos]}
+		default:
+			return state
+	}
 }
